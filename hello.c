896ed5b8de64a62e75053551b1d9656c22e7ce55
Sohaila Ali <ali.sohailakhalid@gmail.com>
2022-10-02 18:36:56 -0400

    Update hello.c

--- hello.c
+++ hello.c
@@ -8,10 +8,4 @@ int main(void)
     // CS50's approach
     string name1 = get_string("What's your name? ");
     printf("Hello, %s\n", name1);
-
-    // Standard C's approach
-    char name[20];
-    printf("What's your name? ");
-    scanf("%s", name);
-    printf("Hello, %s \n", name);
 }
